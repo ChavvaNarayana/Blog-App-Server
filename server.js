1458393cf9f7edd7c74268d3012ef6971f986c07
Chavva <narayanachavva@gail.com>
2023-01-02 22:28:28 +0530

    Server side

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const morgan = require("morgan")
+const cors = require("cors")
+const connectDB = require("./Connection/Connection")
+
+dotenv.config()
+const app = express()
+
+
+app.use(cors())
+app.use(express.json({limit:"5mb"}))
+app.use(morgan("tiny"))
+
+
+app.use("/user", require("./Routes/User"))
+app.use("/blog", require("./Routes/Blogs"))
+
+
+app.listen(process.env.PORT, async () => {
+    await connectDB()
+    console.log(`Server up at port ${process.env.PORT}`);
+})
